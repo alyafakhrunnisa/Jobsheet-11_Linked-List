4084c1407e6a45d150ea9f0a0e257821808ab2e8
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2026-05-06 11:21:22 +0700

    Tugas

---
+++ src/Pertemuan12/NodeAntrian02.java
@@ -0,0 +1,12 @@
+package Pertemuan12;
+
+public class NodeAntrian02 {
+    MahasiswaQueue02 data;
+    NodeAntrian02 next;
+ 
+    public NodeAntrian02(MahasiswaQueue02 data, NodeAntrian02 next) {
+        this.data = data;
+        this.next = next;
+    }
+    
+}
